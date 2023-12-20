129b1cd5ec58e7f5b0311833395815b144aecbc9
SEUNG WON JEONG <SEUNG WON JEONG@DESKTOP-MS92HU8>
2023-12-20 18:13:28 +0900

    Change in master branch

--- MyGit/src/MyGit/YourClass.java
+++ MyGit/src/MyGit/YourClass.java
@@ -3,5 +3,6 @@ package MyGit;
 public class YourClass {
 	public void sayHello(String toWhom) {
 		System.out.println("Hello, " + toWhom);
+		System.out.println("Your change in master branch");
 	}
 }
